5cfb0b9ce37ca43474ca69d8afed795ffdabe3ec
kylemcco <kyle.p.mccormick@gmail.com>
2015-02-25 15:05:21 -0600

    styling updates, fixed set method to update

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,17 +7,18 @@
 	fb.child('games/test').once('value',function (snap) {
 		var data = snap.val();
 
-        $('.panel').on('click', function (event) {
-		  var $panel = $(this);
+    $('.panel').on('click', function (event) {
+	  var $panel = $(this);
 
-	      if ($panel.attr("data-move")) {
-	      	console.log("already played")
-	      } else {
-			  $(this).attr('data-move', 'x');
-	          $(this).append('<div class="move">x</div>');
-	          changeBoardState($panel, data);
-		  }
-		});
+    if ($panel.attr("data-move")) {
+      console.log("already played")
+    } else {
+		  $(this).attr('data-move', 'x');
+      $(this).append('<div class="move">x</div>');
+      changeBoardState($panel, data);
+  	  }
+    });
+
 	}); //end fb.child
 
 	function changeBoardState(panel, data) {
@@ -27,11 +28,11 @@
 	      boardPosition   = {};
 
         boardPosition[position] = $boardPosition;
-	      fbTestGame.set(boardPosition);
+	      fbTestGame.update(boardPosition);
 
         console.log(fbTestGame);
 	      console.log(data);
 
-	};
+	} //end changeBoardState
 
 }()); // end IIFE
