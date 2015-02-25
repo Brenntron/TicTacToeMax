09e51fe423742866de0e67d005d8f869255403e9
Brennan Willingham <brennanw@Brennan-Willinghams-MacBook-Pro.local>
2015-02-25 13:34:42 -0600

    commitin gfor merge

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,18 +1,36 @@
 (function(){
 	'use strict';
 
-	var fb = new Firebase('https://tictactoemax.firebaseio.com/');
+	var fb       = new Firebase('https://tictactoemax.firebaseio.com/')
+	    fbRemove = fb.child('games/test');
 
-	fb.child('games/test');
 
-	$('.panel').on('click', function (event) {
-	  var $panel = $(this);
+	fb.child('games/test').once('value',function (snap) {
+		var data = snap.val();
 
-      if ($panel.attr("data-move")) {
-      	console.log("already played")
-      } else {
-		  $(this).attr('data-move', 'x');
-      $(this).append('<div class="move">x</div>');
-	  }
-	});
-}());
+        $('.panel').on('click', function (event) {
+		  var $panel = $(this);
+
+	      if ($panel.attr("data-move")) {
+	      	console.log("already played")
+	      } else {
+			  $(this).attr('data-move', 'x');
+	          $(this).append('<div class="move">x</div>');
+	          changeBoardState($panel, data);
+		  }
+		});
+	}); //end fb.child
+
+	function changeBoardState(panel, data) {
+	  var boardState      = data.boardState,
+	      $boardPosition  = panel.attr('id'),
+	      boardPosition   = {position: $boardPosition},
+	      fbTestGame      = fb.child('games/test/boardState');
+
+	      fbTestGame.set(boardPosition);
+
+	      console.log(data);
+
+
+	}
+}()); // end IIFE
