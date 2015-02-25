b212aa2f6dd7fb721c010737fc073912dc91289f
kylemcco <kyle.p.mccormick@gmail.com>
2015-02-25 15:44:50 -0600

    fixed alternating turns

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,25 +1,34 @@
 (function(){
 	'use strict';
 
-	var fb = new Firebase('https://tictactoemax.firebaseio.com/');
+	var fb = new Firebase('https://tictactoemax.firebaseio.com/'),
+      counter = 0;
 
 
 	fb.child('games/test').once('value',function (snap) {
 		var data = snap.val();
 
     $('.panel').on('click', function (event) {
-	  var $panel = $(this),
-        xOrO   = 'x';
-
+	  var $panel  = $(this);
 
     if ($panel.attr("data-move")) {
       console.log("already played")
-    } else {
-		  $(this).attr('data-move', 'x');
-      console.log(this);
+    }
+    else if (counter % 2 === 0) {
+      console.log(counter);
+      $(this).attr('data-move', 'x');
       $(this).append('<div class="move">x</div>');
       changeBoardState($panel, data);
-  	  }
+      counter++;
+    }
+    else if (counter % 2 === 1) {
+      console.log('o');
+      $(this).attr('data-move', 'o');
+      $(this).append('<div class="move">o</div>');
+      changeBoardState($panel, data);
+      counter++;
+    }
+
     });
 
 	}); //end fb.child
